7c38b297586879a3a91c66a067de1720d74b1d0f
Artem Vlasiuk <artemvlasiuk@93gmail.com>
2024-05-09 20:52:28 +0300

    make build

---
+++ index.c53fb253.js
@@ -0,0 +1,2 @@
+var e=document.querySelectorAll("table tr");e.forEach(function(t,n){var r,c=t.children[1].textContent;(r=0===n||n===e.length-1?document.createElement("th"):document.createElement("td")).textContent=c,t.insertBefore(r,t.children[4])});
+//# sourceMappingURL=index.c53fb253.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,13 @@
+"use strict";
+const rows = document.querySelectorAll("table tr");
+rows.forEach((row, index)=>{
+    const positionCell = row.children[1];
+    const copiedValue = positionCell.textContent;
+    let newCell;
+    if (index === 0 || index === rows.length - 1) newCell = document.createElement("th");
+    else newCell = document.createElement("td");
+    newCell.textContent = copiedValue;
+    row.insertBefore(newCell, row.children[4]);
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
